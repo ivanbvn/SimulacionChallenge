1d5e87cfa0a853fc61c57e0bc32ea8aa3ed87c25
bilvinas.ivan@gmail.com <bilvinas.ivan@gmail.com>
2022-11-11 01:55:20 -0300

    Ejercicio 09 terminado

--- 09.js
+++ 09.js
@@ -24,7 +24,15 @@ function filtrar(funcion) {
   // productos.filtrar(function(p) {
   //   return p.price >= 50;
   // }) => [{price: 100, name:'tv'}]
-
+  Array.prototype.filtrar = function(callback) {
+    const filteredArray = []
+    this.forEach(function(elemento) {
+      if(callback(elemento)) {
+        filteredArray.push(elemento)
+      }
+    })
+    return filteredArray
+  }
 };
 
 // No modifiques nada debajo de esta linea //
