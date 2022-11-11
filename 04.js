ec34691841baad0f256973cc0a46742f8261e49d
bilvinas.ivan@gmail.com <bilvinas.ivan@gmail.com>
2022-11-10 21:15:15 -0300

    Ejercicio 04 terminado

--- 04.js
+++ 04.js
@@ -13,7 +13,12 @@ function numeroSimetrico(num) {
   // numeroSimetrico(11711) devuelve true
 
   // Tu código:
-
+  var reversedNum = Number(num.toString().split('').reverse().join(''))
+  if (reversedNum === num) {
+    return true
+  } else {
+    return false
+  }
 }
 
 // No modifiques nada debajo de esta linea //
